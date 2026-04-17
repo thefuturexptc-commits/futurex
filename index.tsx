fb173f55783d7d17f1547c480b67360306063804
Khushi <Km056696@gmail.com>
2026-04-17 11:49:43 +0530

    replace Tailwind CDN with local build

--- index.tsx
+++ index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
+import './global.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
