9a6ad3afcba6d7928aab77fbb4c94f5906e73799
Khushi <Km056696@gmail.com>
2026-04-17 11:50:22 +0530

    Revert "replace Tailwind CDN with local build"
    This reverts commit fb173f55783d7d17f1547c480b67360306063804.

--- index.tsx
+++ index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
-import './global.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
